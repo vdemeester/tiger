ec2fbca25be1307acaff0fd318fd3a45f0ea8acf
Vincent Demeester <vincent@sbr.pm>
2018-06-04 17:02:54 +0200

    Update default.nix
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- default.nix
+++ default.nix
@@ -1,20 +1,10 @@
-let
-  _pkgs = import <nixpkgs> {};
-in
-{ pkgs ? import (_pkgs.fetchFromGitHub { owner = "NixOS";
-                                         repo = "nixpkgs-channels";
-                                         rev = "0afb6d789c8bf74825e8cdf6a5d3b9ab8bde4f2d";
-                                         sha256 = "147vhzrnwcy0v77kgbap31698qbda8rn09n5fnjp740svmkjpaiz";
-                                       }) {}
-}:
-
-pkgs.stdenv.mkDerivation rec {
+with import <nixpkgs> {};
+stdenv.mkDerivation  {
     name = "tiger";
-    env = pkgs.buildEnv { name = name; paths = buildInputs; };
     buildInputs = [
-        pkgs.go_1_8
+        pkgs.go_1_10
         pkgs.vndr
         pkgs.gnumake
-	pkgs.gotools
-    ];
+	      pkgs.gotools
+      ];
 }
